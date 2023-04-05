0f6461dcfd776320a9cb79004196f86819f36c9e
riomljr <22552727+riomljr@users.noreply.github.com>
2023-04-05 09:26:38 -0500

    add create and new code to kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,19 +1,35 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(id)
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def edit
   end
 
   def create
+    @kitten = Kitten.create(kitten_params)
+    if @kitten.save
+      flash.notice = "Congrats! You just saved a new Kitten!"
+    else
+      flash.notice = "Oh no! Kitten was not saved, try again"
+      redirect_to new_kitten
+    end
   end
 
   def update
   end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 end
